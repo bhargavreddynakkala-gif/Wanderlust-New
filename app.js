1b26ad718c96e7447f0fb80df025ccd402cdd452
Bhargav Reddy <bhargavreddynakkala@gmail.com>
2026-04-18 19:13:44 +0530

    fixed mongodb connection for using in multiple containers in docker

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req,res,next)=>{
 })
 
 //connecting the mongoose 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlustNew"
+const MONGO_URL="mongodb://mongodb:27017/wanderlustNew"
 async function main(){
    await mongoose.connect(MONGO_URL);
 }
